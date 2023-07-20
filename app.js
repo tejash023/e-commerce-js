e3983ec2bf393ce02a66c09fd818ec94606ffc7c
Tejashwa Rajvardhan <keshu023@gmail.com>
2023-07-20 17:37:14 +0530

    product cart css updated

---
+++ app.js
@@ -0,0 +1,16 @@
+// // Make a GET request to fetch data from a server
+// fetch("https://fakestoreapi.com/products")
+//   .then((response) => {
+//     if (!response.ok) {
+//       throw new Error("Network response was not ok");
+//     }
+//     // Parse the response body as JSON
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     // Handle errors
+//     console.error("Fetch error:", error);
+//   });
